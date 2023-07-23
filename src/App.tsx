cdb778a96daf982aa110efcb538767e73f55e396
kane1213 <kane711213@gmail.com>
2023-07-23 21:13:51 +0800

    add pixi

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 
 import './App.scss'
-export default () => {
+import { Sprite, Stage  } from "react-pixi-fiber";
+import { Texture, Point }from 'pixi.js'
+import bunny from "/images/bunny.png";
+import { useEffect, useState } from 'react';
 
-  return <div className="bg-blue-600 text-white">
+// const bunny: string = 'https://i.imgur.com/IaUrttj.png'
+const Bunny = (props: any) => <Sprite texture={Texture.from(bunny)} {...props} anchor={new Point(0.5, 0.5)} />;
+export default () => {
+  const [rotate, setRotate] = useState<number>(0)
+  useEffect(() => {
+    setInterval(() => {
+      setRotate((prev: number) => prev + .05)
+    }, 50)
+  }, [])
+
+  return <div className=" text-blue-600">
   HELLO WORLD 2
+  <Stage options={{height: 600, width: 600, backgroundColor: '#eee' }}>
+    <Bunny x={290} y={290} rotation={rotate} /> 
+  </Stage>
+
 </div>
 }
