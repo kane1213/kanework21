fa45d8d755dbf0d928dee13c30e59829826a56ba
kane1213 <kane711213@gmail.com>
2023-08-03 10:48:18 +0800

    add router

---
+++ src/lib/util.ts
@@ -0,0 +1,6 @@
+// import { BrowserRouter } from 'react-router-dom';
+
+
+// export const push = (path: string) => {
+//   BrowserRouter.navigate(path);
+// }
---
+++ src/router/index.tsx
@@ -0,0 +1,44 @@
+
+import { lazy } from 'react'
+import { useRoutes, Route, Routes, Navigate } from 'react-router-dom'
+import { Suspense } from 'react'
+
+
+const routes = [
+  { path: '/', key: 'home', ele: lazy(() => import('@/pages/Home')), children: [] },
+  { path: '/game', key: 'game', ele: lazy(() => import('@/pages/Game')), children: [] },
+  { path: '*', key: 'none', ele: lazy(() => import('@/pages/None')), children: [] },
+];
+
+export const routeName: {[key: string ]: string} = routes.reduce((sum, cur) => ({ ...sum, [cur.key]: cur.path, [cur.path]: cur.key }), {})
+
+export default function Index() {
+  const element = useRoutes(
+    routes.map((route: any, index: number) => ({ 
+      path: route.path,
+      element: <route.ele key={index} children={generateSuspectRoutes(route.children)} childlist={route.children.map((child:any) => ({ path: child.path, name: menuName[child.path] } ))} />,
+      ...(!(route.children && route.children.length > 0) 
+        ? {}
+        : {
+            children: route.children.map((routeChild: any, cindex: number) => ({
+              path: routeChild.path,
+              element: <routeChild.ele key={index + '-' + cindex} />
+            }) )
+        })
+    })).concat({
+      path: '/home',
+      element: <Navigate to="/" />
+    })
+  )
+  return element
+}
+
+function generateSuspectRoutes (children: { path: string, ele: any }[]) {
+  return <Suspense fallback={<div className="text-black text-center py-48 ">Loading...</div>}>
+    <Routes>
+      {
+        children.map((child: any) => <Route key={child.path} path={child.path} element={<child.ele />} />)
+      }
+    </Routes>  
+  </Suspense>
+}
