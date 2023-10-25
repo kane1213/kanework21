4d62e6f563d03fd2c4d399549ea98118f8208b7e
kane1213 <kane711213@gmail.com>
2023-10-25 14:16:53 +0800

    add keys

--- src/pages/Music/index.tsx
+++ src/pages/Music/index.tsx
@@ -3,6 +3,7 @@ import { Sprite, Stage, Container } from "react-pixi-fiber";
 import audioData from '@/lib/box-ogg.js';
 import { Midi } from '@tonejs/midi'
 import _ from 'lodash'
+import { Texture } from "pixi.js";
 interface NoteData {
   midi: number,
   name: string,
@@ -28,6 +29,23 @@ export default () => {
   const timeRate: number = 1
   const noteNameGroup = useMemo(() => notes.length > 0 ? _.groupBy(notes, 'name') : {}, [notes])
   const noteTimes = useMemo(() => notes.length > 0 ? _.uniq(_.map(notes, 'time')) : [], [notes])
+  const defaultKeys = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+  const keyboards = {
+    0: defaultKeys.slice(0, 2),
+    1: defaultKeys.slice(),
+    2: defaultKeys.slice(),
+    3: defaultKeys.slice(),
+    4: defaultKeys.slice(),
+    5: defaultKeys.slice(),
+    6: defaultKeys.slice(),
+    7: defaultKeys.slice(),
+    8: defaultKeys.slice(2, 3),
+  }
+
+  const keyNames = Object.entries(keyboards).reduce<string[]>((sum, [num, keys]) => sum.concat(keys.map((key: string) => `${key}${num}`)), [])
+
+  console.log({keyboards, keyNames})
+
   const noteTimeRange = useMemo(() => 
     noteTimes.length > 0
       ? { min: _.head(noteTimes), max: _.last(noteTimes)}
@@ -127,11 +145,11 @@ export default () => {
       clearInterval(process.current.interval)
     }
     
-    process.current.interval = setInterval(() => {
-      process.current.time += 0.5
-      console.log({notes})
-      setCurrentTime(Math.floor(process.current.time))
-    }, 500)
+    // process.current.interval = setInterval(() => {
+    //   process.current.time += 0.5
+    //   console.log({notes})
+    //   setCurrentTime(Math.floor(process.current.time))
+    // }, 500)
   }
 
 
@@ -170,9 +188,9 @@ export default () => {
 
   <div className="mt-2 p-3 flex">
     
-    <div className="titles">
+    {/* <div className="titles">
       {filterdNotes.map((note: string) => <div className="text-xs h-4 text-center border-t-0 first:border-t-[1px] border min-w-[30px]">{note}</div>)}
-    </div>
+    </div> */}
     <div className="flex-1 relative">
       
       {/* <div>
@@ -195,22 +213,24 @@ export default () => {
       </div>
       <div style={{ transform: `translateX(${currentTime * 16}px)` }} className="w-0.5 h-full bg-black absolute top-0 transition-transform ease-linear duration-1000" /> */}
 
-  <Stage ref={stageRef} options={{height: 600, width: 800, background: '#eee' }}>
-      <Container ref={bunnyRef}  >
-
-        {/* {
-          cards.map((card: string, index: number) => {
-            return <Sprite key={card} width={1} height={5} scale={9} x={centerX} y={centerY} texture={Texture.from(`/images/cards/${card}.jpg`)} anchor={{ x: 0.5, y: 0.5 }} zIndex={index} />
-            return null
+  <Stage ref={stageRef} options={{height: 600, width: 800, background: '#aaa' }}>
+      <Container ref={bunnyRef}>
+      {
+        filterdNotes
+          .map((noteName: string, index: number) => {
+            console.log({ noteName })
+            return noteNameGroup.hasOwnProperty(noteName)  
+              ? noteNameGroup[noteName].map((note: any, cIndex: number) => 
+                {
+                  
+                  return <Sprite key={noteName} width={5} height={5} texture={Texture.WHITE} tint="0x000000" x={index * 5} y={cIndex * 5} zIndex={index * 10 + cIndex} />  
+                }
+                )
+              : null
           })
-        } */}
+      }
 
-        {/* {
-          cards.map((card: string, index: number) => {
-            const angle = cardSpacing * index;
-            return <Sprite key={card} width={30} height={50} texture={Texture.from(`/images/cards/${card}.jpg`)} anchor={{ x: 0.5, y: 1 }} rotation={angle * Math.PI / 180} zIndex={index} />
-          })
-        } */}
+
       </Container>
 
     </Stage>
